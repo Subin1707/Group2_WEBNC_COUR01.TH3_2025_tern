5a4dc9cfdbe016b79b75caab58312ba8a867db33
Subin1707 <manhquyen2005xyz@gmail.com>
2026-01-21 03:05:37 +0700

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+/**
+ * @method bool isAdmin()
+ * @method bool isStaff()
+ * @method bool isUser()
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
