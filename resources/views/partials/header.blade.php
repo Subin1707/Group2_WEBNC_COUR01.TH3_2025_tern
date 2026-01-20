4b488ddc590db6c8cf8e16cab9c6757cf07610be
Subin1707 <manhquyen2005xyz@gmail.com>
2026-01-21 03:28:05 +0700

    up

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,8 +16,10 @@
                 <div class="top_1m">
                     <form action="{{ route('movies.index') }}" method="GET" class="input-group">
                         <input type="text" name="search" value="{{ request('search') }}"
-                            class="form-control bg-black text-white" placeholder="Tìm phim...">
-                        <button class="btn text-white bg_red rounded-0 border-0" type="submit">Tìm</button>
+                               class="form-control bg-black text-white" placeholder="Tìm phim...">
+                        <button class="btn text-white bg_red rounded-0 border-0" type="submit">
+                            Tìm
+                        </button>
                     </form>
                 </div>
             </div>
@@ -25,28 +27,10 @@
             <div class="col-md-4">
                 <div class="top_1r text-end">
                     <ul class="social-network social-circle mb-0">
-
                         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                         <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-
-                        {{-- 🔔 CSKH ICON --}}
-                        @auth
-                        <li class="position-relative">
-                            <a href="
-                                @if(Auth::user()->role === 'user')
-                                    {{ route('support.index') }}
-                                @else
-                                    {{ route('support.staff.index') }}
-                                @endif
-                            ">
-                                <i class="fa fa-comments"></i>
-                            </a>
-                        </li>
-                        @endauth
-
-                        {{-- 👤 USER ICON --}}
                         <li>
                             @auth
                                 <a href="{{ route('profile.index') }}">
@@ -58,7 +42,6 @@
                                 </a>
                             @endauth
                         </li>
-
                     </ul>
                 </div>
             </div>
@@ -75,7 +58,7 @@
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent">
+                    data-bs-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -83,44 +66,89 @@
                 <ul class="navbar-nav ms-auto">
 
                     @auth
-                        {{-- ADMIN --}}
+                        {{-- ================= ADMIN ================= --}}
                         @if (Auth::user()->role === 'admin')
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.movies.index') }}">Phim</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.theaters.index') }}">Rạp</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.rooms.index') }}">Phòng</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.showtimes.index') }}">Suất chiếu</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.bookings.index') }}">Đặt vé</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('admin.staffs.index') }}">Tạo Nhân viên</a></li>
-                            <li class="nav-item"><a class="nav-link text-warning" href="{{ route('support.admin.dashboard') }}">🆘 CSKH</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.movies.index') }}">Phim</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.theaters.index') }}">Rạp</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.rooms.index') }}">Phòng</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.showtimes.index') }}">Suất chiếu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.bookings.index') }}">Đặt vé</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.staffs.index') }}">Tạo nhân viên</a>
+                            </li>
 
-                        {{-- USER --}}
+                        {{-- ================= USER / STAFF ================= --}}
                         @else
-                            <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('movies.index') }}">Phim</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('showtimes.index') }}">Suất chiếu</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('theaters.index') }}">Rạp</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('bookings.index') }}">Đặt vé</a></li>
-                            <li class="nav-item"><a class="nav-link text-warning" href="{{ route('support.index') }}">🆘 Hỗ trợ</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('home') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('movies.index') }}">Phim</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('showtimes.index') }}">Suất chiếu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('theaters.index') }}">Rạp</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('bookings.index') }}">Đặt vé</a>
+                            </li>
                         @endif
+
+                        {{-- 🔥 CHĂM SÓC KHÁCH HÀNG --}}
+                        <li class="nav-item">
+                            <a class="nav-link text-warning fw-semibold"
+                               href="{{ route('support.index') }}">
+                                🆘 CSKH
+                            </a>
+                        </li>
 
                         {{-- LOGOUT --}}
                         <li class="nav-item">
                             <form method="POST" action="{{ route('logout') }}" class="d-inline">
                                 @csrf
-                                <button type="submit" class="btn btn-link nav-link text-white">🚪 Đăng xuất</button>
+                                <button type="submit"
+                                        class="btn btn-link nav-link text-white">
+                                    🚪 Đăng xuất
+                                </button>
                             </form>
                         </li>
                     @endauth
 
-                    {{-- GUEST --}}
+                    {{-- ================= GUEST ================= --}}
                     @guest
-                        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('movies.index') }}">Phim</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('showtimes.index') }}">Lịch chiếu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('theaters.index') }}">Rạp</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Đăng nhập</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Đăng ký</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ '/' }}">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('movies.index') }}">Phim</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('showtimes.index') }}">Lịch chiếu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('theaters.index') }}">Rạp</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Đăng nhập</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Đăng ký</a>
+                        </li>
                     @endguest
 
                 </ul>
