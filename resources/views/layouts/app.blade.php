7f9a7acee42be47b2a4444d63e908bdd1b17a006
Subin1707 <manhquyen2005xyz@gmail.com>
2026-01-20 17:19:23 +0700

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,23 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Q&HCinema - Rạp Chiếu Phim Online')</title>
+
+    {{-- CSS --}}
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/global.css') }}" rel="stylesheet">
     <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
-    @stack('styles') 
+
+    @stack('styles')
 </head>
+
 <body class="bg-dark text-white d-flex flex-column min-vh-100">
 
+    {{-- HEADER --}}
     @include('partials.header')
 
-    <main class="container flex-fill py-4">
+    {{-- CONTENT (KHÔNG BỌC container Ở ĐÂY) --}}
+    <main class="flex-fill">
         @yield('content')
     </main>
 
+    {{-- FOOTER --}}
     @include('partials.footer')
 
+    {{-- JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
